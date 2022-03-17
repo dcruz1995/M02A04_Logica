aa328d582f601373f66ce4eac8e0da4554b34963
Fernando Cruz <64991242+dcruz1995@users.noreply.github.com>
2022-03-17 15:40:28 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//atv04
+//Fernando Cruz
+function sextou() {
+  console.log("sextou, mais tu não sextarais");
+}
+sextou();
+function whoIs(name) {
+  console.log(`eu sou ${name}, a primeira de seu nome`);
+}
+whoIs("daenerys");
+function myI(name, year, music) {
+  console.log(
+    `meu nome é ${name} tenho ${year} e eu gosto de escutar ${music}`
+  );
+}
+myI("fernando", "26", "perfect");
+function like(movie, music) {
+  console.log(`eu gosto de assistir ${movie} e ouvir ${music}`);
+}
+like("Procurando nemo", "Whatever is take");
+
+//extra
+function mutiple(valor) {
+  var result = valor * 3;
+  console.log(result);
+}
+mutiple(5);
